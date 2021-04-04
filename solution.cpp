aa322ddb0b49096fcf1c5bc122ec87a757ff1687
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-04-03 17:40:11 -0700

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,36 +1,36 @@
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-vector<int> cpy(int max, string s) {
-    vector<int> temp_arr;
-    for(int i = 0; i < max; i++) {
-        temp_arr.push_back(s[i]);
-    }
-    
-    return temp_arr;
-}
-
 int solution(string s) {
     int answer = s.size();
-    vector<int> vec = cpy(answer, s);
-    vector<int> ans;
-    
-    for(int i = 0; i < answer; i++) {
-        int front_num = 0;
-        if(i+1 <= answer) {
-            if(vec.at(i) == vec.at(i + 1)) {
-                front_num++;
-            } else {
-                if(front_num > 1) {
-                    ans.push_back(front_num);
-                }
-                ans.push_back(vec.at(i));
-                i += front_num;
-            }
+    for(int i = 1; i < answer / 2; i++) {
+      int count = 1;
+      string sub = s.substr(0, i), output;
+
+      for(int j = i; j < answer; j += i) {
+        if(sub == s.substr(j, i)) {
+          count++;
+        } else {
+          if(count > 1) {
+            output += to_string(count);
+            output += sub;
+            sub = s.substr(j, i);
+            count = 1;
+          }
         }
-    } 
-    
-    answer = ans.size();
+      //Check  cout << output << endl;
+      }
+
+    }
     return answer;
+}
+
+int main() {
+
+  int answer = solution("aaabbbacc");
+  cout << answer << endl;
+
+  return 0;
 }
